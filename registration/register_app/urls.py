284eb2a1a41bbbbd88d73e3d3dc0d74e722d9dc0
Aseem Gupta <aseemguptajaipur@gmail.com>
2024-05-02 21:25:15 +0530

    made a signup/login page with access to home page after authentiction only

--- registration/register_app/urls.py
+++ registration/register_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from register_app.views import *
 
 urlpatterns = [
-    path('home_page/',home_page),
-    path('signup_page/',signup_page),
-    path('login_page/',login_page),
+    path('home_page/',home_page,name = "home_page"),
+    path('signup_page/',signup_page,name = 'signup'),
+    path('login_page/',login_page,name = 'login_page'),
+    path('logout/',logout_page,name="logout")
 ]
--- registration/register_app/views.py
+++ registration/register_app/views.py
@@ -1,43 +1,63 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='login_page')
 def home_page(request):
     return render(request,'register_app/home.html')
 
 def signup_page(request):
     if request.method == "POST":
-          data = request.POST
-          first_name= data.get('first_name')
-          last_name = data.get('last_name')
-          user_name =  data.get('user_name')
-          email = data.get('email')
-          password =  data.get('password')
-          print("\n\n\n\n",first_name,last_name,user_name,password,"\n\n\n\n")
+        data = request.POST
+        first_name= data.get('first_name')
+        last_name = data.get('last_name')
+        user_name =  data.get('user_name')
+        email = data.get('email')
+        password =  data.get('password')
+        print("\n\n\n\n",first_name,last_name,user_name,password,"\n\n\n\n")
 
-          user = User.objects.filter(username=user_name)
-          
-          if user.exists():
-               print("user exist")
-            #    messages.error(request, "username already exist.")  
-               return redirect('/register/')
+        user = User.objects.filter(username=user_name)
+        
+        if user.exists():
+            print("user exist")  
+            # return HttpResponse("user already exist")
+            return redirect('signup')
+            # return render(request,'register_app/signup.html')
 
 
-          user = User.objects.create(
-               first_name=first_name,
-               last_name=last_name,
-               username=user_name,
-               email = email
-          )
-          
-          user.set_password(password)
-          user.save()
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=user_name,
+            email = email
+        )
+        
+        user.set_password(password)
+        user.save()
         #   messages.success(request, "account registerd.")
-          return redirect("register_app/login_page/") 
+        return redirect('login_page')
     return render(request,'register_app/signup.html')
 
 def login_page(request):
+    if request.method == "POST":
+        data = request.POST
+        user_name = data.get('user_name')
+        password = data.get('password')
+        user = authenticate(request,username=user_name,password=password)
+        # print(user)
+        if user is not None:
+            context = {'user':user_name}
+            login(request,user)
+            return render(request,'register_app/home.html',context=context)
+
+            # return redirect('home_page')
+        else:
+             print("\n\nusername or password is incorrect\n\n")
+             return redirect('login_page')
+            # return HttpResponse("username or password is incorrect")
     return render(request,'register_app/login.html')
 
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')
